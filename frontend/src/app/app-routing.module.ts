2fb61a81f2420a10a3c0bf6f1be012d38e81f949
Kelvin Helmut <aek6.io@gmail.com>
2020-08-14 10:52:07 -0500

    Guard para creditos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { SolicitudComponent } from './pages/solicitud/solicitud.component';
 import { ConfirmacionComponent } from './pages/confirmacion/confirmacion.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: SolicitudComponent},
   {path: 'confirmacion', component: ConfirmacionComponent},
-  {path: 'creditos', component: CreditosComponent},
+  {path: 'creditos', component: CreditosComponent, canActivate: [AuthGuard]},
   {path: 'credito', component: CreditoComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registro', component: RegistroComponent},
